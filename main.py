97a0d97299bbb95ad33a86c1d310f1668b4c546d
Yasmin Chavez <ychavez2014ig@gmail.com>
2019-10-16 11:22:50 -0600

    download

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tweepy
 import csv #Import csv
+import time
+import pandas as pd
+
 
 consumer_key = "0fGnBSc3yWClYpEW1g8fLioQj"
 consumer_secret = "uVfnOaKGln5J8XpYVIVMGFj9cgFef2hbgUujtAZYWoItLmw8eG"
@@ -14,20 +17,28 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth) 
 
 # Open/create a file to append data to
-csvFile = open('result.csv', 'a')
+#csvFile = open('result.csv', 'a')
 #Use csv writer
-csvWriter = csv.writer(csvFile)
+#csvWriter = csv.writer(csvFile)
 
-auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search,
-                           q = "google",
+tweets = tweepy.Cursor(api.search,
+                           q = "#TraficoGT  -filter:retweets",
                            since = "2018-01-01",
                            until = "2018-12-31",
-                           lang = "en").items():
+                           lang = "en").items(10)
 
     # Write a row to the CSV file. I use encode UTF-8
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-    print tweet.created_at, tweet.text
-csvFile.close()
+    #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    #print (tweet.created_at, tweet.text)
+
+
+info = [[tweet.user.screen_name, tweet.user.location, tweet.created_at, tweet.text] for tweet in tweets]
+
+tweet_info = pd.DataFrame(data=info, 
+                    columns=['user', "location", "created_at", "texto"])
+print(tweet_info)
+
+#csvFile.close()
